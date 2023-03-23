0741a571bd5d3cd55b430d634baa78e75dd0e7a1
Lucas Safeno <lugnorio@gmail.com>
2023-03-23 19:18:43 -0300

    Curso de Java 13: Operadores (Aritiméticos, Lógicos e Relacionais

---
+++ javaBasico_loiane/src/com/atualtech/cursojava/aula13/OperadoresAritmetricos.java
@@ -0,0 +1,38 @@
+package com.atualtech.cursojava.aula13;
+
+public class OperadoresAritmetricos {
+
+	public static void main(String[] args) {
+		
+		// Operadores Aritimetricos 
+		int resultado  = 1 + 2;
+		System.out.println(resultado);
+		
+		resultado = resultado - 1;
+		System.out.println(resultado);
+		
+		resultado  = resultado * 2;
+		System.out.println(resultado);
+		
+		resultado = resultado / 2;
+		System.out.println(resultado);
+		
+		resultado = resultado + 8;
+		System.out.println(resultado);
+		
+		resultado = resultado % 7;
+		System.out.println(resultado);
+		
+		String primeiroNome = "Lucas";
+		String segundoNome = " Tenório";
+		String terceiraString = primeiroNome + segundoNome;
+		System.out.println(terceiraString);
+		
+		// adicionar +1
+		resultado++;
+		System.out.println(resultado);
+
+
+	}
+
+}
---
+++ javaBasico_loiane/src/com/atualtech/cursojava/aula13/OperadoresRelacionais.java
@@ -0,0 +1,16 @@
+package com.atualtech.cursojava.aula13;
+
+public class OperadoresRelacionais {
+	public static void main(String[] args) {
+		// Operadores Relacionais
+		int valor1 = 1;
+		int valor2 = 2;
+		
+		System.out.println("Valor1 == Valor2 = " + (valor1 == valor2));
+		System.out.println("Valor1 != Valor2 = " + (valor1 != valor2));
+		System.out.println("Valor1 > Valor2 = " + (valor1 > valor2));
+		System.out.println("Valor1 < Valor2 = " + (valor1 < valor2));
+		System.out.println("Valor1 >= Valor2 = " + (valor1 >= valor2));
+		System.out.println("Valor1 >= Valor2 = " + (valor1 <= valor2));
+	}
+}
