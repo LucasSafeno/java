66534d9697f6197d89d227cee5fe228cb34bef5c
Lucas Safeno <lugnorio@gmail.com>
2023-12-30 13:40:29 -0300

    Curso de Java 06: Passando argumentos para o programa

--- javaBasico_loiane/src/com/atualtech/cursojava/aula11/SequencialEscape.java
+++
@@ -1,8 +0,0 @@
-package com.atualtech.cursojava.aula11;
-
-public class SequencialEscape {
-	public static void main(String[] args) {
-		System.out.println("\"Hello, World\"");
-	}
-
-}
--- javaBasico_loiane/src/javaBasico_loiane/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package javaBasico_loiane;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-
-}
