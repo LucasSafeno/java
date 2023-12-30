ec7725d7b3b2257e7b8eb6e49bc1606994cdfbed
Lucas Safeno <lugnorio@gmail.com>
2023-12-30 10:39:18 -0300

    Java&Cia - Universidade XTI - Java - 008 - Garbage Collector

---
+++ universidadeXTI/08-Introdução_Garbage_Collector/GarbageCollector.java
@@ -0,0 +1,10 @@
+public class GarbageCollector{
+	public static voic main(String[] args){
+	// Variavel Primitiva
+		int x = 7;
+		
+	// Variavel de Referencia
+	String y = "XTI";
+	y = "xti.com.br";
+	y = null;
+}
