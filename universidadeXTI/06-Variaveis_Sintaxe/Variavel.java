567d59621e9b155b9c87e5dc8a3b366a85164b14
Lucas Safeno <lugnorio@gmail.com>
2023-12-30 10:15:01 -0300

    Java&Cia - Universidade XTI - Java - 006 - Variaveis e Sintaxe

---
+++ universidadeXTI/06-Variaveis_Sintaxe/Variavel.java
@@ -0,0 +1,17 @@
+/** 
+*	Apresentar os conceitos da variavel
+*	@author LucasSafeno
+*/
+public class Variavel{
+	public static void main(String[] args){
+		
+		String nome, segundoNome, ultimoNome;
+		nome = "Lucas";
+		segundoNome = "Tenório";
+		ultimoNome	= "Cavalcanti";
+		int idade = 29;
+		boolean casado = true;
+		
+		System.out.println(nome);
+	}
+}
