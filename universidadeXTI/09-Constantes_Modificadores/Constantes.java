c75f26ca34cddf8534cdb0ed239c2cf1d1885255
Lucas Safeno <lugnorio@gmail.com>
2023-12-30 13:23:27 -0300

    Java&Cia - Universidade XTI - Java - 009 - Constantes e Modificador final

---
+++ universidadeXTI/09-Constantes_Modificadores/Constantes.java
@@ -0,0 +1,14 @@
+public class Constantes{
+	public static void main(String[] args){
+		// Variavel
+		int populacaoBrasileira = 203429773;
+		// Constante 
+		 // final declara uma constante. Não pode ser modificado
+		final double PI = 3.14159265358979323846;
+		final char SEXO_MASCULINO = 'M';
+		final char SEXO_FEMENINO = 'F';
+		
+		System.out.println(PI);
+		
+	}
+}
