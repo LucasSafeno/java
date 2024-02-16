4d64cce9afb73a29d9e7cd2d5bf79feeb1365039
Lucas Safeno <lugnorio@gmail.com>
2024-02-16 13:37:46 -0300

    DevDojo - 09 - Comentários

--- maratona-java/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,16 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Comentario doc
+ * Pode utilizar <b>html</b>
+ * ativar modo rendeizado : CTRL + ALT + Q
+ */
 public class OlaDevDojo {
+    // Isso é um comentário de uma linha
+    /*
+     *   comentários de multiplcas linhas
+     *   várias linhas
+     */
     public static void main(String[] args) {
         System.out.println("Olá mundo");
     }
