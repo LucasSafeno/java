d503e89806c733e1d1d9cca417ef7579af2c71b1
Lucas Safeno <lugnorio@gmail.com>
2024-02-16 14:07:55 -0300

    DevDojo - 14 - Tipos primitivos pt 05 - Exercício

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+/*
+    Crie variaveis para os campos descritos abaixo entre <>e imprima a mensagem:
+    Eu <nome>, morando no endereço <endereço>
+    confirmo que recebi o salário de <salario> na data <data>
+ */
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String nome = "Lucas";
+        String endereco = "Jaboatão dos Guararapes";
+        double salario = 2500D;
+        String data = "16 de fevereiro de 2024";
+
+        System.out.println("Eu " + nome + " morando no endereço : " + endereco + " confirmo que recebi o salário de " +
+                salario + " reais");
+    }
+}
