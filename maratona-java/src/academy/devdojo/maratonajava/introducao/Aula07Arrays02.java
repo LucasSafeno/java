f6c24836b39ffc4856bf824763d67e619efdd635
Lucas Safeno <lugnorio@gmail.com>
2024-02-20 15:07:43 -0300

    DevDojo JAVA - 34 - Arrays pt 03

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -2,14 +2,22 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays02 {
     public static void main(String[] args) {
-        // Valores padrões tipos primitivos
-        // byte,short,int, long,float e doble = 0
-        // char = '\u0800' -> espaço em branco
-        // boolean = false
-        // String = null
-        float[] idades = new float[3];
-        System.out.println(idades[0]);
-        System.out.println(idades[1]);
-        System.out.println(idades[2]);
+
+        // tamanho não aumenta dinamicamente
+        String[] nomes = new String[3];
+        nomes[0] = "Thauanna";
+        nomes[1] = "Lucas";
+        nomes[2] = "Eysis";
+        //nomes[3] = "Tenório";
+
+        for (int i = 0; i < nomes.length; i++){
+            System.out.println(nomes[i]);
+        }
+        System.out.println("=========================");
+        nomes = new String[4];
+        for (int i = 0; i < nomes.length; i++){
+            System.out.println(nomes[i]);
+        }
+
     }
 }
