b919d05041c49cfdd65f9b8bb0e429c1432e65a4
Lucas Safeno <lugnorio@gmail.com>
2024-02-20 14:57:39 -0300

    DevDojo JAVA - 33 - Arrays pt 02

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -7,7 +7,7 @@ public class Aula07Arrays01 {
         idades[0] = 21;
         idades[1] = 15;
         idades[2] = 11;
-        
+
         System.out.println(idades[0]);
         System.out.println(idades[1]);
         System.out.println(idades[2]);
---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // Valores padrões tipos primitivos
+        // byte,short,int, long,float e doble = 0
+        // char = '\u0800' -> espaço em branco
+        // boolean = false
+        // String = null
+        float[] idades = new float[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
